3bd568e3dff2ddb4d2bd4ac38d412a816cacdc99
antigones <antigones@users.noreply.github.com>
2019-12-10 17:28:11 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,31 +1,29 @@
 window.onload = () => {
 	var cy = cytoscape({
-	  container: document.getElementById('cy'), // container to render in
+	container: document.getElementById('cy'), // container to render in
 	layout: {
-						name: 'cola'
-					},
-
-					style: [
-						{
-							selector: 'node',
-							css: {
-								'background-color': '#f92411',
-								 'label': 'data(label)'
-							}
-						},
-
-						{
-							selector: 'edge',
-							css: {
-								'line-color': '#f92411',
-								'label': 'data(label)'
-							}
-						}
-					],
+		name: 'cola'
+		},
+		style: [
+			{
+				selector: 'node',
+				css: {
+					'background-color': '#f92411',
+					 'label': 'data(label)'
+				}
+			},
+			{
+				selector: 'edge',
+				css: {
+					'line-color': '#f92411',
+					'label': 'data(label)'
+					}
+			}
+			],
 	});
 	this.cy = cy;
-    this.action = new Action(this);
-    // Call setCallbacks to register assistant action callbacks.
-    this.action.setCallbacks();
-  window.focus();
+	this.action = new Action(this);
+    	// Call setCallbacks to register assistant action callbacks.
+    	this.action.setCallbacks();
+  	window.focus();
 };
