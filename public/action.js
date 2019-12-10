be33073db8854c4ae76eb87ca248309e8d31cf96
antigones <antigones@users.noreply.github.com>
2019-12-10 17:36:48 +0100

    Update action.js

--- public/action.js
+++ public/action.js
@@ -1,21 +1,21 @@
 class Action {
 
-  constructor(graphContext) {
-    this.canvas = window.interactiveCanvas;
-	const that = this;
-	this.graphContext = graphContext;
-    this.commands = {
-	  ADDOBJECT: function(data) {
-        that.addNode(data.label);
-      },
-	  ADDEDGE: function(data) {
-        that.addEdge(data.from, data.to, data.label);
-      },
-      DEFAULT: function() {
-        // do nothing, when no command is found
-      },
-    };
-  }
+	constructor(graphContext) {
+		this.canvas = window.interactiveCanvas;
+		const that = this;
+		this.graphContext = graphContext;
+		this.commands = {
+			ADDOBJECT: function(data) {
+				that.addNode(data.label);
+			},
+			ADDEDGE: function(data) {
+				that.addEdge(data.from, data.to, data.label);
+			},
+			DEFAULT: function() {
+				// do nothing, when no command is found
+			},
+		};
+	}
 
   /**
    * Register all callbacks used by Google Assistant Action
@@ -26,30 +26,28 @@ class Action {
     const that = this;
     // declare assistant canvas action callbacks
     const callbacks = {
-      onUpdate(data) {
-        that.commands[data.command ? data.command.toUpperCase() :
-          'DEFAULT'](data);
-      },
+	    onUpdate(data) {
+		    that.commands[data.command ? data.command.toUpperCase() : 'DEFAULT'](data);
+	    },
     };
     // called by the Interactive Canvas web app once web app has loaded to
     // register callbacks
-
     this.canvas.ready(callbacks);
   }
   
   addNode(label) {
-		this.graphContext.cy.add({
-			group: 'nodes',
-			data: { id:label, label: label, weight: 10}
-		});
+	  this.graphContext.cy.add({
+		  group: 'nodes',
+		  data: { id:label, label: label, weight: 10}
+	  });
 	this.graphContext.cy.fit();
 	this.canvas.sendTextQuery(`Node added`);
   }
   
    addEdge(from, to, label) {
-		this.graphContext.cy.add({
-			group: 'edges', data: { id: from+"_"+to, source: from, target: to, label: label }
-		});
-	this.canvas.sendTextQuery(`Edge added`);
-  }
+	   this.graphContext.cy.add({
+		   group: 'edges', data: { id: from+"_"+to, source: from, target: to, label: label }
+	   });
+	   this.canvas.sendTextQuery(`Edge added`);
+   }
 }
